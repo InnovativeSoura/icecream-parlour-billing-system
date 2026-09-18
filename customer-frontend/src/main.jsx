0f4684e7cbd6933b5eca62935d33fda3deed07c6
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-09-18 21:25:48 +0530

    Add separate customer frontend

--- customer-frontend/src/main.jsx
+++ customer-frontend/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
 import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 
 import "./index.css";
 
@@ -11,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
