014237ae5ceba2e213e2cfb7e92f869f263e7e89
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-09-18 15:35:22 +0530

    Add separate customer frontend

--- customer-frontend/src/Layout/Layout.jsx
+++ customer-frontend/src/Layout/Layout.jsx
@@ -23,7 +23,7 @@ import {
   FaCalendarAlt,
 } from "react-icons/fa";
 
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "./../context/AuthContext";
 
 import "./Layout.css";
 
